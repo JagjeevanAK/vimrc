d84f3b45ec434da4879d5c071cc53c68e623d972
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-08-09 21:05:22 +0530

    added toggle term
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- lua/jagjeevan/lazy/fterm.lua
+++ lua/jagjeevan/lazy/fterm.lua
@@ -1,20 +1,19 @@
 -- Alternative terminal plugin - more minimal and focused
 -- Uncomment this if you prefer FTerm over ToggleTerm
---[[
-return {
-    'numToStr/FTerm.nvim',
-    config = function()
-        require'FTerm'.setup({
-            border = 'double',
-            dimensions  = {
-                height = 0.9,
-                width = 0.9,
-            },
-        })
 
-        -- Terminal keymaps similar to undotree style
-        vim.keymap.set('n', '<leader>tt', '<CMD>lua require("FTerm").toggle()<CR>')
-        vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-    end
+return {
+    -- 'numToStr/FTerm.nvim',
+    -- config = function()
+    --     require'FTerm'.setup({
+    --         border = 'double',
+    --         dimensions  = {
+    --             height = 0.9,
+    --             width = 0.9,
+    --         },
+    --     })
+
+    --     -- Terminal keymaps similar to undotree style
+    --     vim.keymap.set('n', '<leader>tt', '<CMD>lua require("FTerm").toggle()<CR>')
+    --     vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    -- end
 }
---]]
