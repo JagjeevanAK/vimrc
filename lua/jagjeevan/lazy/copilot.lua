6617a93ea99dedcdad96a974f8a846b6163df6e7
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-10-26 18:56:05 +0530

    updated key
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- lua/jagjeevan/lazy/copilot.lua
+++ lua/jagjeevan/lazy/copilot.lua
@@ -6,7 +6,6 @@ return{
       -- Copilot keymaps
 	  vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("\\<Tab>")', { silent = true, expr = true, script = true })
 	  vim.g.copilot_no_tab_map = true
-	  vim.api.nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", { silent = true })
       vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", { silent = true })
     end
   }
