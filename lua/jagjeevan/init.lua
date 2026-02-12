f94fadb01c6b7c51367bcd729f7a6f380a119fa1
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2026-02-12 19:17:33 +0530

    feat: Added markdown renderer
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- lua/jagjeevan/init.lua
+++ lua/jagjeevan/init.lua
@@ -70,6 +70,8 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         vim.opt_local.wrap = true
         vim.opt_local.linebreak = true
+        vim.opt_local.conceallevel = 2
+        vim.opt_local.concealcursor = "nc"
     end,
 })
 
